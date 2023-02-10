6e70e0f6ded9acc16e85416f97b7569c8a779244
Artem Gromakov <pandawithmelon@gmail.com>
2023-02-10 20:34:53 +0300

    Add iframe, add initial HTML snippet for iframe, connect it to parent window through event listener. Implement transfering bundled code from parent to iframe through postMessage method. Implement resetting iframe to default HTML snippet after each submit.

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ const App = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
   const ref = useRef<esbuild.Service>();
+  const iframe = useRef<any>();
   useEffect(() => {
     startService();
   }, []);
@@ -26,18 +27,43 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+    //Reset iframe before each bundling
+    iframe.current.srcDoc = html;
+    //Bundle code
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
       plugins: [unpkgPlugin(), fetchPlugin(input)],
       define: {
-        'process.env.NODE.ENV': "'production'",
+        'process.env.NODE_ENV': "'production'",
         global: 'window',
       },
     });
-    setCode(result.outputFiles[0].text);
+
+    // setCode(result.outputFiles[0].text);
+    //Post a message from parent window to iframe, containing bundled code
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = ` <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (e) => {
+          try {
+            eval(e.data);
+          } catch(err) {
+            const root = document.getElementById('root');
+            root.innerHTML = '<div>' + err + '</div>';
+            console.error(err);
+          }
+          
+        }, false);
+      </script>
+    </body>
+  </html>`;
 
   return (
     <div>
@@ -51,6 +77,7 @@ const App = () => {
         <button type="submit">Submit</button>
       </form>
       <pre>{code}</pre>
+      <iframe title="preview" ref={iframe} srcDoc={html} sandbox="allow-scripts"></iframe>
     </div>
   );
 };
