f63a88f854155d521ed5bba6006e599cec3cbb97
Artem Gromakov <pandawithmelon@gmail.com>
2023-02-17 15:32:22 +0300

    Refactor: extract bundler code from components

---
+++ src/bundler/index.ts
@@ -0,0 +1,27 @@
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPlugin } from '../plugin/unpkPlugin';
+import { fetchPlugin } from '../plugin/fetchPlugin';
+
+let service: esbuild.Service;
+const bundle = async (rawCode: string) => {
+  if (!service) {
+    service = await esbuild.startService({
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+      worker: true,
+    });
+  }
+  const result = await service.build({
+    entryPoints: ['index.js'],
+    bundle: true,
+    write: false,
+    plugins: [unpkgPlugin(), fetchPlugin(rawCode)],
+    define: {
+      'process.env.NODE_ENV': "'production'",
+      global: 'window',
+    },
+  });
+
+  return result.outputFiles[0].text;
+};
+
+export default bundle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,18 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPlugin } from './plugin/unpkPlugin';
-import { fetchPlugin } from './plugin/fetchPlugin';
 import CodeEditor from './components/code-editor';
 import Iframe from './components/iframe';
+import bundle from './bundler';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const App = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
-  const ref = useRef<esbuild.Service>();
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-      worker: true,
-    });
-  };
 
   const onChange = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    //Bundle code
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': "'production'",
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const res = await bundle(input);
+    setCode(res);
   };
 
   return (
