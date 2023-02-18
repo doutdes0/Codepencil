1f710b0f0411620d415d5b254b806e77894fdb5b
Artem Gromakov <pandawithmelon@gmail.com>
2023-02-18 18:58:57 +0300

    Fix issues with resizing: set max resize height, keep drag event from transfering inside iframe.

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -13,7 +13,8 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       height={400}
       width={1000}
       resizeHandles={['s']}
-      maxConstraints={[1000, 1000]}
+      maxConstraints={[1000, window.innerHeight * 0.9]}
+      minConstraints={[Infinity, 24]}
     >
       {children}
     </ResizableBox>
