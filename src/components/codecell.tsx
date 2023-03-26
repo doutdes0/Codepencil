0dafbea9fd9d8cad6c668d12cdf541883bec06af
Artem Gromakov <pandawithmelon@gmail.com>
2023-03-26 21:56:42 +0400

    Extract cumulativeCode into separate hook

--- src/components/codecell.tsx
+++ src/components/codecell.tsx
@@ -4,6 +4,7 @@ import Iframe from './iframe';
 import Resizable from './resizable';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useCumulativeCode } from '../hooks/use-cumulaive-code';
 import { Cell } from '../state';
 import './codecell.css';
 
@@ -13,42 +14,7 @@ interface CodeCellProps {
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
-
-  const cumulativeCode = useTypedSelector((state) => {
-    const { order, data } = state.cells;
-    const orderedList = order.map((id) => data[id]);
-    const cumulativeCode = [
-      `
-    import _React from 'react';
-    import _ReactDOM from 'react-dom';
-    const _root = _ReactDOM.createRoot(root);
-    const show = (val) => {
-      if(typeof val === 'object') {
-        if(val.$$typeof && val.props) {
-          _root.render(val);
-        } else {
-        root.innerHTML = JSON.stringify(val);
-        }
-      } else {
-        root.innerHTML = val;
-      }
-    }
-    `,
-    ];
-
-    for (let c of orderedList) {
-      if (c.id === cell.id) {
-        cumulativeCode.push(c.content);
-        break;
-      } else {
-        const regex = /show(.*)/g;
-        cumulativeCode.push(c.content.replace(regex, ''));
-      }
-    }
-    console.log(cumulativeCode);
-    return cumulativeCode.join('\n');
-  });
-
+  const cumulativeCode = useCumulativeCode(cell.id);
   useEffect(() => {
     if (!bundle) {
       createBundle(cell.id, cell.content);
---
+++ src/hooks/use-cumulaive-code.ts
@@ -0,0 +1,38 @@
+import { useTypedSelector } from './use-typed-selector';
+
+export const useCumulativeCode = (cellId: string) => {
+  return useTypedSelector((state) => {
+    const { order, data } = state.cells;
+    const orderedList = order.map((id) => data[id]);
+    const cumulativeCode = [
+      `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
+        const _root = _ReactDOM.createRoot(root);
+        const show = (val) => {
+          if(typeof val === 'object') {
+            if(val.$$typeof && val.props) {
+              _root.render(val);
+            } else {
+            root.innerHTML = JSON.stringify(val);
+            }
+          } else {
+            root.innerHTML = val;
+          }
+        }
+        `,
+    ];
+
+    for (let c of orderedList) {
+      if (c.id === cellId) {
+        cumulativeCode.push(c.content);
+        break;
+      } else {
+        const regex = /show(.*)/g;
+        cumulativeCode.push(c.content.replace(regex, ''));
+      }
+    }
+    console.log(cumulativeCode);
+    return cumulativeCode.join('\n');
+  });
+};
