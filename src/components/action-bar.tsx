c7fd7252fd295084507d3dcae8beba7d0b6db08c
Artem Gromakov <pandawithmelon@gmail.com>
2023-03-28 20:16:37 +0400

    Add action-bar divider

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,4 +1,5 @@
 import { useActions } from '../hooks/use-actions';
+import { Fragment } from 'react';
 import './action-bar.css';
 
 interface ActionBarProps {
@@ -9,38 +10,41 @@ interface ActionBarProps {
 const ActionBar: React.FC<ActionBarProps> = ({ id, type }) => {
   const { moveCell, deleteCell } = useActions();
   return (
-    <div className="action-bar-wrapper">
-      <div className="mark">
-        <span className="icon">
-          {type === 'code' && <i className="fas fa-code"></i>}
-          {type === 'text' && <i className="fas fa-pencil-alt"></i>}
-        </span>
+    <Fragment>
+      <div className="action-bar-wrapper">
+        <div className="mark">
+          <span className="icon">
+            {type === 'code' && <i className="fas fa-code"></i>}
+            {type === 'text' && <i className="fas fa-pencil-alt"></i>}
+          </span>
+        </div>
+        <button
+          title="Move cell up"
+          onClick={() => moveCell(id, 'up')}
+        >
+          <span className="icon">
+            <i className="fas fa-angle-double-up"></i>
+          </span>
+        </button>
+        <button
+          title="Move cell down"
+          onClick={() => moveCell(id, 'down')}
+        >
+          <span className="icon">
+            <i className="fas fa-angle-double-down"></i>
+          </span>
+        </button>
+        <button
+          title="Delete cell"
+          onClick={() => deleteCell(id)}
+        >
+          <span className="icon">
+            <i className="fas fa-times"></i>
+          </span>
+        </button>
       </div>
-      <button
-        title="Move cell up"
-        onClick={() => moveCell(id, 'up')}
-      >
-        <span className="icon">
-          <i className="fas fa-angle-double-up"></i>
-        </span>
-      </button>
-      <button
-        title="Move cell down"
-        onClick={() => moveCell(id, 'down')}
-      >
-        <span className="icon">
-          <i className="fas fa-angle-double-down"></i>
-        </span>
-      </button>
-      <button
-        title="Delete cell"
-        onClick={() => deleteCell(id)}
-      >
-        <span className="icon">
-          <i className="fas fa-times"></i>
-        </span>
-      </button>
-    </div>
+      <div className="action-bar-divider"></div>
+    </Fragment>
   );
 };
 
