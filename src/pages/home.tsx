0cef55e3102430d32835cf306c2594a30c8914ab
Artem Gromakov <pandawithmelon@gmail.com>
2023-04-02 23:03:11 +0400

    Add home page content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,60 @@
-import CodeCell from '../components/CodeCell';
-import { Cell } from '../state';
+import showcase1 from '../assests/showcase1.png';
+import showcase2 from '../assests/showcase2.png';
+import { Link } from 'react-router-dom';
 import './home.css';
 
 const Home = () => {
-  const heroCell: Cell = {
-    id: '1',
-    type: 'code',
-    content: 'const foo="bar"',
-  };
   return (
-    <div className="hero">
-      <div className="hero-story">
-        <h2>Welcome to Codepencil</h2>
-      </div>
-      <div className="hero-card">
-        <div className="hero-showcase">
-          <CodeCell cell={heroCell} />
+    <>
+      <div className="hero-wrapper">
+        <h2>Welcome to Codepencil.</h2>
+        <div className="hero hero-1">
+          <div className="hero-story">
+            <h3>Create, build fast and behold the beauty of your work!</h3>
+            <p>
+              Simply import any package from npm, given it's optimized for in-browser bundling, and
+              you're ready to go. <br /> We've already preinstalled <strong>React</strong> and{' '}
+              <strong>ReactDOM</strong> for you! <br />
+              You don't have to set it up, just use the <code>show{'(<App/>)'}</code> function to
+              display components and other data types. <br />
+              And, most conviniently, variables from previous codecells can be accessed in following
+              ones.
+            </p>
+          </div>
+          <div className="hero-card hero-card-1">
+            <div className="hero-showcase">
+              <img
+                src={showcase1}
+                alt="editor-showcase"
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="hero-wrapper">
+        <div className="hero hero-2">
+          <div className="hero-card hero-card-2">
+            <div className="hero-showcase">
+              <img
+                src={showcase2}
+                alt="editor-showcase"
+              />
+            </div>
+          </div>
+          <div className="hero-story">
+            <h3>Supply your code with useful notes!</h3>
+            <p>
+              Writing notes is as easy as creating a README file with the help of inbuilt markdown
+              editor. <br />
+              This way you won't ever lose your train of thought.
+            </p>
+            <Link to="/celllist">
+              <button className="action-btn">Dive in!</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
