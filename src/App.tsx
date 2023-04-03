650fe35bfa51e902aba6debf4f220aa46028b9cc
Artem Gromakov <pandawithmelon@gmail.com>
2023-04-03 15:33:05 +0400

    Minor style changes, add scroll to top on <Link/> clicks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navbar from './components/NavBar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import Footer from './components/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './styles/styles.css';
@@ -10,6 +10,7 @@ const App = () => {
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollRestoration />
     </div>
   );
 };
