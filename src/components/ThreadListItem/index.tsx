30e956a73f0226fea16d92db1111d9292aca72d1
Artem Gromakov <pandawithmelon@gmail.com>
2023-04-16 19:10:23 +0400

    Add edit and delete buttons to thread list items, styles

--- src/components/ThreadListItem/index.tsx
+++ src/components/ThreadListItem/index.tsx
@@ -1,6 +1,7 @@
 import { Thread } from '../../state/reducers/threadsReducer';
 import { useNavigate } from 'react-router-dom';
 import './thread-item.css';
+import { useActions } from '../../hooks/use-actions';
 
 interface ThreadListItemProps {
   thread: Thread;
@@ -8,6 +9,8 @@ interface ThreadListItemProps {
 
 const ThreadListItem: React.FC<ThreadListItemProps> = ({ thread: { name, description, id } }) => {
   const navigate = useNavigate();
+  const { deleteThread } = useActions();
+
   return (
     <div
       onClick={() => navigate('/cellList', { state: { threadID: id } })}
@@ -15,6 +18,24 @@ const ThreadListItem: React.FC<ThreadListItemProps> = ({ thread: { name, descrip
     >
       <h3>{name}</h3>
       {description && <p>{description}</p>}
+      <div className="action-buttons">
+        <button title="Edit thread">
+          <span className="icon">
+            <i className="fas fa-sm fa-pencil-alt"></i>
+          </span>
+        </button>
+        <button
+          title="Delete thread"
+          onClick={(e) => {
+            e.stopPropagation();
+            deleteThread(id);
+          }}
+        >
+          <span className="icon">
+            <i className="fas fa-times"></i>
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
